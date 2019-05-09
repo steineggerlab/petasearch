5b8ee07c1319296dee7bf48239135d3de4943da8
Jonas Hügel <huegel.jonas@gmail.com>
2019-05-09 10:30:19 +0200

    refactored variable names to be more descriptive

--- src/sra/compare2kmertables.cpp
+++ src/sra/compare2kmertables.cpp
@@ -6,42 +6,37 @@
 #include "MathUtil.h"
 
 
-#define KMER_TABLE_1 "tmp/kmerTableDecodedAsLong"
-#define KMER_TABLE_2 "tmp/kmerTableDecodedAsLong2"
-
 size_t kmer2index(long kmer);
 
 int compare2kmertables(int argc, const char **argv, const Command& command){
     Parameters& par = Parameters::getInstance();
     par.spacedKmer = false;
     par.parseParameters(argc, argv, command, 1, false);
-    
 
+    FILE* handleQuerryKmerTable = fopen(argv[0],"rb");
+    int fdQuerryTable = fileno(handleQuerryKmerTable);
+    struct stat fileStatsQuerryTable;
+    fstat(fdQuerryTable, &fileStatsQuerryTable);
+    size_t fileSizeQuerryTable = fileStatsQuerryTable.st_size;
 
-    FILE* handleKmerTable1 = fopen(argv[0],"rb");
-    int fdTable1 = fileno(handleKmerTable1);
-    struct stat fileStatsTable1;
-    fstat(fdTable1, &fileStatsTable1);
-    size_t fileSizeTable1 = fileStatsTable1.st_size;
+    FILE* handleTargetKmerTable = fopen(argv[1],"rb");
+    int fdTargetTable = fileno(handleTargetKmerTable);
+    struct stat fileStatsTargetTable;
+    fstat(fdTargetTable,&fileStatsTargetTable);
+    size_t fileSizeTargetTable = fileStatsTargetTable.st_size;
 
-    FILE* handleKmerTable2 = fopen(argv[1],"rb");
-    int fdTable2 = fileno(handleKmerTable2);
-    struct stat fileStatsTable2;
-    fstat(fdTable2,&fileStatsTable2);
-    size_t fileSizeTable2 = fileStatsTable2.st_size;
-
-    long* posKmerTable1 = (long*) mmap(NULL, fileSizeTable1, PROT_READ,MAP_PRIVATE, fdTable1, 0);
-    long* posKmerTable2 = (long*) mmap(NULL, fileSizeTable2, PROT_READ,MAP_PRIVATE, fdTable2, 0);
-    if (posix_madvise (posKmerTable1, fileSizeTable1, POSIX_MADV_SEQUENTIAL|POSIX_MADV_WILLNEED) != 0){
+    long* startPosQuerryTable = (long*) mmap(NULL, fileSizeQuerryTable, PROT_READ,MAP_PRIVATE, fdQuerryTable, 0);
+    long* startPosTargetTable = (long*) mmap(NULL, fileSizeTargetTable, PROT_READ,MAP_PRIVATE, fdTargetTable, 0);
+    if (posix_madvise (startPosQuerryTable, fileSizeQuerryTable, POSIX_MADV_SEQUENTIAL|POSIX_MADV_WILLNEED) != 0){
         Debug(Debug::ERROR) << "posix_madvise returned an error for k-mer table 2 " << "\n";
     } 
-    if (posix_madvise (posKmerTable2, fileSizeTable2, POSIX_MADV_SEQUENTIAL|POSIX_MADV_WILLNEED) != 0){
+    if (posix_madvise (startPosTargetTable, fileSizeTargetTable, POSIX_MADV_SEQUENTIAL|POSIX_MADV_WILLNEED) != 0){
         Debug(Debug::ERROR)  << "posix_madvise returned an error for k-mer table 2 " << "\n";
     }
 
-    long* currentQuerryPos = posKmerTable1;
-    long* currentTargetPos = posKmerTable2;
-    long* endTargetPos = posKmerTable2 + fileSizeTable2/sizeof(long);
+    long* currentQuerryPos = startPosQuerryTable;
+    long* currentTargetPos = startPosTargetTable;
+    long* endTargetPos = startPosTargetTable + fileSizeTargetTable/sizeof(long);
     size_t equalKmers = 0;
 
     while(currentTargetPos <= endTargetPos){
@@ -59,16 +54,13 @@ int compare2kmertables(int argc, const char **argv, const Command& command){
             ++currentTargetPos;
         }
     }
+    munmap(startPosQuerryTable, fileSizeQuerryTable);
+    munmap(startPosTargetTable, fileSizeTargetTable);
+    fclose(handleQuerryKmerTable);
+    fclose(handleTargetKmerTable);
+
     Debug(Debug::INFO)<<"number of equal Kmers: "<<equalKmers<<"\n";
-
-    munmap(posKmerTable1, fileSizeTable1);
-    munmap(posKmerTable2, fileSizeTable2);
-    fclose(handleKmerTable1);
-    fclose(handleKmerTable2);
-
     return equalKmers;
-
-
 }
 
 
