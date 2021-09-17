dff6d81617596933c3048f2587adcec6e60cde26
matchy233 <matchy233@gmail.com>
2021-09-17 16:01:03 +0900

    Squashed 'lib/block-aligner/' changes from 8f04560..ce9407c
    ce9407c Only build on stable in CI
    3c0628a Remove compiling C API in CI
    7dd6d4f Remove macos ci
    e5f84db Set library path for linking in C API
    beed557 Remove windows from CI
    ef4ee84 Finished updating everything to use feature flags
    d990007 Work in progress on making block aligner easier to use
    574dd56 Updated experiments yet again for paper
    
    git-subtree-dir: lib/block-aligner
    git-subtree-split: ce9407c0f46418b11599b271b7ecd3b2daa42175

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@
 //!
 //! ## Example
 //! ```
-//! # #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"))]
-//! # {
 //! use block_aligner::scan_block::*;
 //! use block_aligner::scores::*;
 //! use block_aligner::cigar::*;
@@ -22,29 +20,12 @@
 //!
 //! assert_eq!(res, AlignResult { score: 7, query_idx: 24, reference_idx: 21 });
 //! assert_eq!(a.trace().cigar(res.query_idx, res.reference_idx).to_string(), "2M6I16M3D");
-//! # }
 //! ```
 //!
 //! When building your code that uses this library, it is important to specify the
-//! correct flags to turn on specific target features that this library uses.
-//!
-//! For x86 AVX2:
-//! ```text
-//! RUSTFLAGS="-C target-cpu=native" cargo build --release
-//! ```
-//! or
-//! ```text
-//! RUSTFLAGS="-C target-feature=+avx2" cargo build --release
-//! ```
-//!
-//! For WASM SIMD:
-//! ```text
-//! RUSTFLAGS="-C target-feature=+simd128" cargo build --target=wasm32-wasi --release
-//! ```
+//! correct feature flags: `simd_avx2` or `simd_wasm`.
 
-#![feature(core_intrinsics)]
-#![feature(asm)]
-#![feature(vec_into_raw_parts)]
+#![cfg_attr(feature = "mca", feature(asm))]
 
 //use wee_alloc::WeeAlloc;
 
@@ -54,39 +35,33 @@
 // special SIMD instruction set modules adapted for this library
 // their types and lengths are abstracted out
 
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"))]
+#[cfg(feature = "simd_avx2")]
 #[macro_use]
 #[doc(hidden)]
 /// cbindgen:ignore
 pub mod avx2;
 
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"))]
+#[cfg(feature = "simd_avx2")]
 pub use avx2::L;
 
-#[cfg(all(target_arch = "wasm32", target_feature = "simd128"))]
+#[cfg(feature = "simd_wasm")]
 #[macro_use]
 #[doc(hidden)]
 /// cbindgen:ignore
 pub mod simd128;
 
-#[cfg(all(target_arch = "wasm32", target_feature = "simd128"))]
+#[cfg(feature = "simd_wasm")]
 pub use simd128::L;
 
-#[cfg(any(
-        all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"),
-        all(target_arch = "wasm32", target_feature = "simd128")
-))]
+#[cfg(any(feature = "simd_avx2", feature = "simd_wasm"))]
 pub mod scan_block;
-
-#[cfg(any(
-        all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"),
-        all(target_arch = "wasm32", target_feature = "simd128")
-))]
+#[cfg(any(feature = "simd_avx2", feature = "simd_wasm"))]
 pub mod scores;
-
+#[cfg(any(feature = "simd_avx2", feature = "simd_wasm"))]
 pub mod cigar;
+#[cfg(any(feature = "simd_avx2", feature = "simd_wasm"))]
 pub mod simulate;
 
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"))]
+#[cfg(feature = "simd_avx2")]
 #[doc(hidden)]
 pub mod ffi;
